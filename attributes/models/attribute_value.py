f0da957911990e5612a370b970b9e4fe4f227099
MykolaBerkosha <mberkosa@gmail.com>
2022-02-17 14:45:55 +0200

    Fix bugs.

--- attributes/models/attribute_value.py
+++ attributes/models/attribute_value.py
@@ -2,7 +2,6 @@
 import six
 
 from django.db import models
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from attributes.managers import AttributeValueManager
