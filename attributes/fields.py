16d08d7a1f6075c93d46a698351275e9ccb741bd
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-04-29 13:22:30 +0300

    Fix issues.

--- attributes/fields.py
+++ attributes/fields.py
@@ -1,13 +1,25 @@
 
-from basement.fields import FormField
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from attributes.widgets import AttributesFormFieldWidget
 from attributes.forms import AttributesForm
 
 
-class AttributesFormField(FormField):
+class AttributesFormField(forms.Field):
 
     widget = AttributesFormFieldWidget
+    form = None
 
-    def _build_form(self, *args, **kwargs):
-        return AttributesForm(*args, **kwargs)
+    def init_form(self, *args, **kwargs):
+        self.form = AttributesForm(*args, **kwargs)
+        self.widget.form = self.form
+
+    def clean(self, *args, **kwargs):
+        if not self.form.is_valid():
+            raise forms.ValidationError(_('Form is invalid.'))
+
+        return self.form.cleaned_data
+
+    def commit(self, *args, **kwargs):
+        return self.form.commit(*args, **kwargs)
--- attributes/models/__init__.py
+++ attributes/models/__init__.py
@@ -2,4 +2,3 @@
 from attributes.models.attribute import Attribute
 from attributes.models.attribute_option import AttributeOption
 from attributes.models.attribute_value import AttributeValue, VALUE_FIELDS
-from attributes.models.service import AttributeService
